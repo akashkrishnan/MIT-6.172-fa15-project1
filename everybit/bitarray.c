86123614e3d69477eb02baaeae9b4f7ffedba278
Akash Krishnan <akashk16@mit.edu>
2015-09-18 14:03:53 -0400

    reduced memory access overhead in bitarray_reverse [0.001352s]

--- everybit/bitarray.c
+++ everybit/bitarray.c
@@ -180,10 +180,14 @@ void bitarray_rotate(bitarray_t *const bitarray,
 static void bitarray_reverse(bitarray_t *const bitarray,
                              size_t start,
                              size_t stop) {
+  size_t a;
+  size_t b;
   while(start < stop) {
-    bitarray_set(bitarray, start, bitarray_get(bitarray, start) ^ bitarray_get(bitarray, stop));
-    bitarray_set(bitarray, stop,  bitarray_get(bitarray, start) ^ bitarray_get(bitarray, stop));
-    bitarray_set(bitarray, start, bitarray_get(bitarray, start) ^ bitarray_get(bitarray, stop));
+    b = bitarray_get(bitarray, stop);
+    a = bitarray_get(bitarray, start) ^ b;
+    b = a ^ b;
+    bitarray_set(bitarray, stop,  b);
+    bitarray_set(bitarray, start, a ^ b);
     start++;
     stop--;
   }
